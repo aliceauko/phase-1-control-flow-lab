46911d8d36ce1fff7619fae4ecdcade848176ed0
aliceauko <alice.auko@student.moringaschool.com>
2022-06-07 02:25:10 +0300

    commit tree 33d0b1b361b0968e1b3fdc8e027b97df65271599

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let reply;
+  if(distance<= 400){
+    reply ='This one is on me!'
+  } else if (distance > 2500) {
+    reply = 'No can do.'
+  } else {
+    reply =  'I will gladly take your thirty bucks.'
+  }
+   return reply
+
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response;
+  return response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.'
+  }
+  return response;
 }
